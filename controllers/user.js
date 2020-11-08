2c18ba0600f4d29e02f82a2ad0d13f75daffeb20
JessicaFournier <jessica.fournier59@gmail.com>
2020-11-08 17:13:33 +0100

    status erreur pour différentiation

--- controllers/user.js
+++ controllers/user.js
@@ -54,7 +54,7 @@ exports.login = (req, res, next) => {
                     )
                 });
             })
-        .catch(error => res.status(500).json({ error }));
+        .catch(error => res.status(400).json({ error }));
     })
     .catch(error => res.status(500).json({ error }));
 };  
