1d8c8dee4cf725dac01eaed2c7304439969a7e47
JessicaFournier <jessica.fournier59@gmail.com>
2020-11-30 17:25:00 +0100

    modification email hashé

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,8 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const MaskData = require('maskdata');
 
+const hash = require('hash.js');
+
 //définit comment l'adresse mail doit être masquée
 const maskEmail2Options = {
     maskWith: "*", 
@@ -14,47 +16,48 @@ const maskEmail2Options = {
 //sauvegarde un nouvel utilisateur, masque l'email et hash le mot de passe
 
 exports.signup = (req, res, next) => {
+    const emailHash = hash.sha256().update(req.body.email).digest('hex');
     // hachage du mot de passe, salage par 10
     bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-            const user = new User( 
-            {
-                email: req.body.email,
-                emailMasked: MaskData.maskEmail2(req.body.email, maskEmail2Options),
-                password: hash
-            });
-            // sauvegarde de l'utilisateur
-            user.save()
-            .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-            .catch(error => res.status(400).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
+    .then(hash => {
+        const user = new User( 
+        {
+            email: emailHash,
+            password: hash
+        });
+        // sauvegarde de l'utilisateur
+        user.save()
+        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+        .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
 };
 
 
 //vérifie si l'utilisateur existe, si oui, on vérifie le mot de passe. Si celui ci est correct, on renvoie un tokenn contenant l'ID de l'utilisateur
 //sinon on renvoie une erreur
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            if (!user) {
-            return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+    const emailHash = hash.sha256().update(req.body.email).digest('hex');
+    User.findOne({ email:emailHash })
+    .then(user => {
+        if (!user) {
+        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+        }
+        bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+            if (!valid) {
+            return res.status(401).json({ error: 'Mot de passe incorrect !' });
             }
-            bcrypt.compare(req.body.password, user.password)
-            .then(valid => {
-                if (!valid) {
-                return res.status(401).json({ error: 'Mot de passe incorrect !' });
-                }
-                res.status(200).json({
-                    userId: user._id,
-                    token: jwt.sign(
-                      { userId: user._id },
-                      'RANDOM_TOKEN_SECRET',
-                      { expiresIn: '24h' }
-                    )
-                });
-            })
-        .catch(error => res.status(400).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
+            res.status(200).json({
+                userId: user._id,
+                token: jwt.sign(
+                  { userId: user._id },
+                  'RANDOM_TOKEN_SECRET',
+                  { expiresIn: '24h' }
+                )
+            });
+        })
+    .catch(error => res.status(400).json({ error }));
+})
+.catch(error => res.status(500).json({ error }));
 };  
--- models/user.js
+++ models/user.js
@@ -6,7 +6,6 @@ const uniqueValidator = require('mongoose-unique-validator');
 const userSchema = mongoose.Schema({
     userId: { type: String, required: false },
     email: { type: String, required: true, unique: true },
-    emailMasked: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
 
